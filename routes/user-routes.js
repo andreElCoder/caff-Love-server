75c1bc9475583a9047c7ff621c6f989fb7cbfaff
Rabaca <andre.rabaca@nokia.com>
2020-07-03 20:07:59 +0100

    Rating added

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,6 +1,7 @@
 const express    = require('express');
 const userRoutes = express.Router();
 const User = require ('../models/user-model');
+const { response } = require('express');
 
 
 userRoutes.get('/searchUser',(req,res)=>{
@@ -9,6 +10,11 @@ User.find({"username": { "$regex": req.body.search, "$options": "i" }},{"usernam
   .then(response =>{
     res.status(200).json(response)
   })
+})
+
+userRoutes.get('/profile',(req,res)=>{
+  User.find({"username": req.body.username})
+  res.status(200).json(response)
 })
 
 module.exports = userRoutes
