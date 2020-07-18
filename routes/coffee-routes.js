fd385b33b7a11a6840d78dc72e6a6338c0b2f5d6
Rabaca <andre.rabaca@nokia.com>
2020-07-18 15:46:14 +0100

    Added coffees/:username

--- routes/coffee-routes.js
+++ routes/coffee-routes.js
@@ -22,11 +22,7 @@ coffeeRoutes.get('/coffees/:username' ,(req,res)=>{
     User.find({"username": req.params.username})
     .then(responseFromAPI =>{
         if(responseFromAPI){
-            //res.status(200).json(responseFromAPI.data[0])
-            Coffee.find({'_id':{ $in: responseFromAPI.data[0]}})
-            .then(response =>{
-                res.status(200).json(response)
-            })
+        res.status(200).json(responseFromAPI.data[0])
     }
         else{res.status(404).json({message: "No coffee found"})}
     })
